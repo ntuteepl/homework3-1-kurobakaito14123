1168ab0b9a277f127a0e4169cac03fbd7e5f6864
kurobakaito14123 <146057501+kurobakaito14123@users.noreply.github.com>
2023-10-19 00:18:32 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char ans[5], str[5]; int a = 0, b = 0;
+	scanf("%s", &ans);
+	while (a != 4) {
+		a = 0; b = 0;
+		scanf("%s", &str);
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				if (str[i] == ans[j])
+				{
+					if (i == j) a++;
+					else b++;
+				}
+			}
+		}
+		printf("%dA%dB\n", a, b);
+	}
+	while (a != 0) scanf("%d", &a);
+}
+
 
