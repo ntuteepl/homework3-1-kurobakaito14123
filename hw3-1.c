e7299b3d04df9d8425d9bae92f2993a1d946cc22
kurobakaito14123 <146057501+kurobakaito14123@users.noreply.github.com>
2023-10-19 00:17:49 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+main()
+{
+int n,k=1;
+scanf("%d", &n);
+for(int i=2;(i*i)<=n;i++)
+    if((n%i)==0)
+        k=0;
+if(k==1)
+    printf("YES");
+if(k==0)
+    printf("NO");
+}
+
+
 
