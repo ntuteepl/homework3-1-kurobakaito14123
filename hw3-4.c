1f704e4ef6ac041103a6a1ea41c43e439a8117e2
kurobakaito14123 <146057501+kurobakaito14123@users.noreply.github.com>
2023-10-24 12:23:43 +0800

    Update hw3-4.c

--- hw3-4.c
+++ hw3-4.c
@@ -3,10 +3,10 @@
 int main()
 {
 	char ans[5], str[5]; int a = 0, b = 0;
-	scanf("%s", &ans);
+	scanf("%s", ans);
 	while (a != 4) {
 		a = 0; b = 0;
-		scanf("%s", &str);
+		scanf("%s", str);
 		for (int i = 0; i < 4; i++)
 		{
 			for (int j = 0; j < 4; j++)
