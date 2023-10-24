9031f6b1954d4520ac59d33634a6104cb497c865
kurobakaito14123 <146057501+kurobakaito14123@users.noreply.github.com>
2023-10-24 12:24:10 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -4,7 +4,7 @@ int main()
 {
 	char str1[10], str2[10];
 	int i = 0, n1 = 0, n2 = 0;
-	scanf("%s %s", &str1, &str2);
+	scanf("%s %s", str1, str2);
 	while (str1[i] != 0 || str2[i] != 0) {
 		n1 += str1[i] & 0xf;
 		n2 += str2[i] & 0xf;
